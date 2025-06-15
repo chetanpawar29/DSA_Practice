fee6d8b04db524b9401e991132005ddda23cb76b
Chetan Pawar <163544059+chetanpawar29@users.noreply.github.com>
2025-06-15 11:01:13 +0530

    Time: 3 ms (75.19%) | Memory: 51.1 MB (6.95%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,21 +1,44 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-
-        List<Integer> list = new ArrayList<>();
+        List<Integer> list  = new LinkedList<>();
         int n = nums.length;
+        int ele1 = 0, ele2 = 0;
+        int ctr1 = 0, ctr2 = 0;
 
-        Map<Integer,Integer> map = new HashMap<>();
-
-        for(int ele : nums){
-            int value = map.getOrDefault(ele, 0);
-            map.put(ele, value+1);
-        }
-
-        for(Map.Entry<Integer,Integer> e : map.entrySet()){
-            if(e.getValue() > n/3){
-                list.add(e.getKey());
+        for(int i=0; i<n; i++){
+            if(ctr1 == 0 && nums[i] != ele2){
+                ctr1 = 1;
+                ele1 = nums[i];
+            }else if(ctr2 == 0 && nums[i] != ele1){
+                ctr2 = 1;
+                ele2 = nums[i];
+            }else if(ele1 == nums[i]){
+                ctr1++;
+            }else if(ele2 == nums[i]){
+                ctr2++;
+            }else{
+                ctr1--;
+                ctr2--;
             }
         }
+        
+        ctr1=0; ctr2=0;
+        for(int i=0; i<n; i++){
+            if(ele1 == nums[i]){
+                ctr1++;
+            }else if(ele2 ==nums[i]){
+                ctr2++;
+            }
+        }
+        
+
+         if(ctr1 > n/3){
+            list.add(ele1);
+        }
+        if(ctr2 > n/3){
+            list.add(ele2);
+        }
         return list;
+
     }
 }
