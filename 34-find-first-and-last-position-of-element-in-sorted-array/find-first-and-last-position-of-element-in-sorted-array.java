09351026b052f593fe48adc3700a096bde57caa0
Chetan Pawar <163544059+chetanpawar29@users.noreply.github.com>
2025-06-24 23:42:03 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (93.42%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,18 +1,52 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int n = nums.length;
-        int arr[] = new int[2];
-        Arrays.fill(arr,-1);
+    
+    public int firstOccurance(int nums[],int n, int x){
+        int low = 0;
+        int high = n-1;
+        int first = -1;
 
-        for(int i=0; i<n; i++){
-            if(nums[i] == target){
-                if(arr[0] == -1){
-                    arr[0] = i;
-                }
-                arr[1] = i;
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(nums[mid] == x){
+                first = mid;
+                high = mid-1;
+            }else if(nums[mid] > x){
+                high = mid-1;
+            }else{
+                low = mid+1;
             }
         }
-        
+        return first;
+    }
+
+    public int lastOccurance(int nums[],int n, int x){
+        int low = 0;
+        int high = n-1;
+        int last = -1;
+
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(nums[mid] == x){
+                last = mid;
+                low = mid+1;
+            }else if(nums[mid] > x){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return last;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int n = nums.length;
+
+        int first = firstOccurance(nums, n, target);
+        int last = lastOccurance(nums, n, target);
+
+        int arr[] = {first, last};
+
         return arr;
+        
     }
 }
