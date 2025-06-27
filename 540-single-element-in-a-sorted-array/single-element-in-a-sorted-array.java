bc36c050c15ebe6b5752b5086d3584c62aef567e
Chetan Pawar <163544059+chetanpawar29@users.noreply.github.com>
2025-06-27 09:57:44 +0530

    Time: 2 ms (9.84%) | Memory: 50.3 MB (66.18%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -11,35 +11,37 @@ class Solution {
 
         //Better T.C -> o(n) s.c->O(n/2)
 
-        Map<Integer, Integer> map = new HashMap<>();
+        // Map<Integer, Integer> map = new HashMap<>();
 
-        for(int i=0; i<n; i++){
-            int value = map.getOrDefault(nums[i], 0);
-            map.put(nums[i], value+1);
-        }
+        // for(int i=0; i<n; i++){
+        //     int value = map.getOrDefault(nums[i], 0);
+        //     map.put(nums[i], value+1);
+        // }
 
-        for(Map.Entry<Integer, Integer> it : map.entrySet()){
-            if(it.getValue() == 1){
-                return it.getKey();
-            }
-        }
+        // for(Map.Entry<Integer, Integer> it : map.entrySet()){
+        //     if(it.getValue() == 1){
+        //         return it.getKey();
+        //     }
+        // }
 
-        return -1;
+        // return -1;
 
 
         // Brute force T.C -> O(n^2)
 
-        // for(int i=0; i<n-1; i++){
-        //     int count = 0;
-        //     for(int j=i+1; j<n; j++){
-        //         if(nums[i] == nums[j]){
-        //             count++;
-        //         }
-        //     }
-        //     if(count == 0){
-        //         return nums[i];
-        //     }
-        // }
-        // return -1;
+        for(int i=0; i<n; i+=2){
+            int num = nums[i];
+            int count = 1;
+            for(int j=i+1; j<n; j++){
+                if(nums[j] == num){
+                    count++;
+                    break;
+                }
+            }
+            if(count == 1){
+                return num;
+            }
+        }
+        return -1;
     }
 }
