de88a299a2d81c25dff41d5a56550d3403dc58c4
Chetan Pawar <163544059+chetanpawar29@users.noreply.github.com>
2025-06-27 09:50:24 +0530

    Time: 17 ms (5.03%) | Memory: 53.9 MB (6.15%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -3,28 +3,28 @@ class Solution {
         int n = nums.length;
 
         // Optimal (XOR concept) t.c->o(n)
-        int xor = 0;
-        for(int i=0; i<n; i++){
-            xor = xor^nums[i];
-        }
-        return xor;
+        // int xor = 0;
+        // for(int i=0; i<n; i++){
+        //     xor = xor^nums[i];
+        // }
+        // return xor;
 
         //Better T.C -> o(n) s.c->O(n/2)
 
-        // Map<Integet, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
 
-        // for(int i=0; i<n; i++){
-        //     int value = map.getOrDefault(nums[i], 0);
-        //     map.put(nums[i], value+1);
-        // }
+        for(int i=0; i<n; i++){
+            int value = map.getOrDefault(nums[i], 0);
+            map.put(nums[i], value+1);
+        }
 
-        // for(Map.Entry<Integer, Integer> it : map.entrySet()){
-        //     if(it.getValue == 0){
-        //         return it.getKey();
-        //     }
-        // }
+        for(Map.Entry<Integer, Integer> it : map.entrySet()){
+            if(it.getValue() == 1){
+                return it.getKey();
+            }
+        }
 
-        // return -1;
+        return -1;
 
 
         // Brute force T.C -> O(n^2)
