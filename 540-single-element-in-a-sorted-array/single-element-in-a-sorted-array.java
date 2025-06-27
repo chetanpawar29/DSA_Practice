34d6771a6a8a2365fcb9849724fc8890998aae11
Chetan Pawar <163544059+chetanpawar29@users.noreply.github.com>
2025-06-27 09:47:51 +0530

    Time: 1 ms (25.29%) | Memory: 50 MB (90.64%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+
+        // Optimal (XOR concept) t.c->o(n)
+        int xor = 0;
+        for(int i=0; i<n; i++){
+            xor = xor^nums[i];
+        }
+        return xor;
+
+        //Better T.C -> o(n) s.c->O(n/2)
+
+        // Map<Integet, Integer> map = new HashMap<>();
+
+        // for(int i=0; i<n; i++){
+        //     int value = map.getOrDefault(nums[i], 0);
+        //     map.put(nums[i], value+1);
+        // }
+
+        // for(Map.Entry<Integer, Integer> it : map.entrySet()){
+        //     if(it.getValue == 0){
+        //         return it.getKey();
+        //     }
+        // }
+
+        // return -1;
+
+
+        // Brute force T.C -> O(n^2)
+
+        // for(int i=0; i<n-1; i++){
+        //     int count = 0;
+        //     for(int j=i+1; j<n; j++){
+        //         if(nums[i] == nums[j]){
+        //             count++;
+        //         }
+        //     }
+        //     if(count == 0){
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
+    }
+}
