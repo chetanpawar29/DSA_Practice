6ea7bec18bca7f218db817b77118b88dd9528cde
Chetan Pawar <163544059+chetanpawar29@users.noreply.github.com>
2025-06-29 09:14:53 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (54.21%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,14 +1,5 @@
 class Solution {
     public int mySqrt(int n) {
-         int ans = 0;
-        for(long i=1; i<=n; i++){
-            long val = i * i;
-            if(val <= (long) n){
-                ans =(int) i;
-            }else{
-                break;
-            }
-        }
-        return ans;
+        return (int) Math.sqrt(n);
     }
 }
