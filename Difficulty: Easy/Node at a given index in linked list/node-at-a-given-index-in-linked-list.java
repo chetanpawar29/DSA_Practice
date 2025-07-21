aba9b968ac0470512bac194aefdb7eb78d54bb04
Chetan Pawar <163544059+chetanpawar29@users.noreply.github.com>
2025-07-21 11:21:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Node at a given index in linked list/node-at-a-given-index-in-linked-list.java
@@ -0,0 +1,30 @@
+/*node class of the linked list
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    public int GetNth(Node head, int index) {
+        // Code here
+        if(head == null) return -1;
+        
+        Node temp = head;
+        int i = 0;
+        while(temp != null){
+            i++;
+            if(i == index){
+                return temp.data;
+            }
+            temp = temp.next;
+        }
+        return -1;
+    }
+}
