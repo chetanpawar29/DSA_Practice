714c9564824c98fab9770ae83353ccf12e5382d7
Chetan Pawar <163544059+chetanpawar29@users.noreply.github.com>
2025-11-07 10:39:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.java
@@ -0,0 +1,20 @@
+class Solution {
+    public char nonRepeatingChar(String s) {
+        // code here
+        int n = s.length();
+        int hashArr[] = new int[26];
+        
+        for(int i=0; i<n; i++){
+            hashArr[s.charAt(i) - 'a']++;
+        }
+        
+        for(int i=0; i<n; i++){
+            if(hashArr[s.charAt(i) - 'a'] == 1){
+                return (char) (97 + s.charAt(i) - 'a');
+            }
+            
+            
+        }
+        return '$';
+    }
+}
