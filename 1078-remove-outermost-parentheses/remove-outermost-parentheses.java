ccac1c724b0214673ba842ccca1d7a18b4c620bc
Chetan Pawar <163544059+chetanpawar29@users.noreply.github.com>
2026-01-02 19:36:34 +0530

    Time: 8 ms (21.29%) | Memory: 43.7 MB (39.70%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '('){
+                if(!stack.isEmpty()) sb.append(ch);
+                stack.push(ch);
+            }else{
+                stack.pop();
+                if(!stack.isEmpty()) sb.append(ch);
+                
+            }
+        }
+
+        return sb.toString();
+    }
+}
