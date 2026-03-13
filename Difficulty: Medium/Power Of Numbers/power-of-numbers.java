b09f94ed33e932a9d416c9dd946c148246e92a0a
Chetan Pawar <163544059+chetanpawar29@users.noreply.github.com>
2026-03-13 13:24:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Power Of Numbers/power-of-numbers.java
+++ Difficulty: Medium/Power Of Numbers/power-of-numbers.java
@@ -1,8 +1,9 @@
 class Solution {
-    public int reverseExponentiation(int n) {
+    public static int reverseExponentiation(int n) {
         // code here
         int ori = n;
         int rev = 0;
+        
         while(n > 0){
             int ld = n%10;
             rev = rev*10+ld;
