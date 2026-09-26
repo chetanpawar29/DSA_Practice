b9e66c2c530e0e47eb64dd50810f4352432debdc
Chetan Pawar <163544059+chetanpawar29@users.noreply.github.com>
2026-09-26 17:34:59 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (51.24%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,52 +1,46 @@
 class Solution {
-    
-    public int firstOccurance(int nums[],int n, int x){
-        int low = 0;
-        int high = n-1;
+    public int firstOccurance(int[] nums, int target)
+    {
         int first = -1;
+        int high = nums.length-1;
+        int low = 0;
 
         while(low <= high){
-            int mid = (low+high)/2;
-            if(nums[mid] == x){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target){
                 first = mid;
                 high = mid-1;
-            }else if(nums[mid] > x){
-                high = mid-1;
+            }else if(nums[mid] > target){
+                high = mid -1;
             }else{
                 low = mid+1;
             }
         }
         return first;
     }
-
-    public int lastOccurance(int nums[],int n, int x){
-        int low = 0;
-        int high = n-1;
+    public int lastOccurance(int[] nums, int target)
+    {
         int last = -1;
+        int high = nums.length-1;
+        int low = 0;
 
         while(low <= high){
-            int mid = (low+high)/2;
-            if(nums[mid] == x){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target){
                 last = mid;
                 low = mid+1;
-            }else if(nums[mid] > x){
-                high = mid-1;
+            }else if(nums[mid] > target){
+                high = mid -1;
             }else{
                 low = mid+1;
             }
         }
         return last;
     }
-
     public int[] searchRange(int[] nums, int target) {
-        int n = nums.length;
+        int first = firstOccurance(nums, target);
+        int last = lastOccurance(nums, target);
 
-        int first = firstOccurance(nums, n, target);
-        int last = lastOccurance(nums, n, target);
-
-        int arr[] = {first, last};
-
-        return arr;
-        
+        return new int[]{first, last};
     }
 }
